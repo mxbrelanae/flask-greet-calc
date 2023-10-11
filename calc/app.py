5d844931630bc458efbf0e46b76fcf8e82bc31d3
Bre Dale <bredale122@gmail.com>
2023-10-11 13:16:31 -0400

    adding completed apps

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,64 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route("/add")
+def ab_add():
+    """Add a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
+
+@app.route("/sub")
+def ab_sub():
+    """Subtract and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+
+    return str(result)
+
+@app.route("/mult")
+def ab_mult():
+    """Multiply a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+
+    return str(result)
+
+@app.route("/div")
+def ab_div():
+    """Divide a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+
+    return str(result)
+
+#PART II
+
+operators = {
+        "add": add,
+        "sub": sub,
+        "mult": mult,
+        "div": div,
+        }
+
+@app.route("/math/<oper>")
+def ab_math(ops):
+    """Do math on a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[ops](a, b)
+
+    return str(result)
